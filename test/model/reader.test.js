eb278c0af9f2fadbf505b9d7dea3c4837a420f74
Andrei Tsygankov <andrei.tsygankov@gmail.com>
2018-07-21 02:00:07 +0300

    + voxels test

--- test/model/reader.test.js
+++ test/model/reader.test.js
@@ -1,9 +1,17 @@
 import { readModel } from "../../src/model/reader";
 
-test("Coord ctor", () => {
+test("readModel", () => {
 
   let m = readModel('./problemsL/LA001_tgt.mdl')
   expect(m.r).toBe(20);
-  expect(m.isFilled(0, 0, 0)).toBe(false);
+
+  let fn = 0;
+  for(let x = 0; x < m.r; x++)
+  	for(let y = 0; y < m.r; y++)
+  		for(let z = 0; z < m.r; z++)
+  			if(m.isFilled(x, y, z))
+  				fn++;
+
+  expect(fn).toBe(511);
 
 });
