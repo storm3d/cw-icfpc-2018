95f5e78b53d732d08aa8a60dcb7acdb96dd1bfe8
Dmitry Boyko <dmitry@thebodva.com>
2018-07-21 15:12:02 +0300

    update eslint and upload

--- upload_icfp.sh
+++ upload_icfp.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -e
 privateID=e196b3421b5141ed8763a5837d0612ed
+timestamp=$(date +"%H%M%S")
+filename=submission$timestamp.zip
 cd $1
-zip --password $privateID submission.zip *
-mv submission.zip .. && cd ..
-sha=$(shasum -a 256 submission.zip | awk '{ print $1 }')
-scp submission.zip bodva@thebodva.com:/var/www/thebodva.com/www/upload
+zip --password $privateID $filename *
+mv $filename .. && cd ..
+sha=$(shasum -a 256 $filename | awk '{ print $1 }')
+scp $filename bodva@thebodva.com:/var/www/thebodva.com/www/upload
 curl -L \
   --data-urlencode action=submit \
   --data-urlencode privateID=$privateID \
-  --data-urlencode submissionURL=https://thebodva.com/upload/submission.zip \
+  --data-urlencode submissionURL=https://thebodva.com/upload/$filename \
   --data-urlencode submissionSHA=$sha \
   https://script.google.com/macros/s/AKfycbzQ7Etsj7NXCN5thGthCvApancl5vni5SFsb1UoKgZQwTzXlrH7/exec
+rm -rf $filename
