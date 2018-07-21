70b2552c1dd83b9a283b932909b210da485e25d4
Dmytro Bochorishvili <dbochorishvili@smartling.com>
2018-07-21 21:29:50 +0300

    fix Void toString

--- src/model/command.js
+++ src/model/command.js
@@ -2,7 +2,7 @@
 import { State } from "./model";
 
 export class Trace {
-  constructor(state : State) {
+  constructor(state: State) {
     this.state = state;
     this.commands = [];
   }
@@ -15,7 +15,7 @@ export class Trace {
   }
 
   execCommands(arr: Array) {
-    for(let c of arr) {
+    for (let c of arr) {
       this.execCommand(c)
     }
   }
@@ -34,48 +34,48 @@ export class Trace {
 }
 
 export class Halt {
-  run(state : State, bid : number) {
+  run(state: State, bid: number) {
     let bot = state.getBot(bid)
 
-    if(bot.pos.x !== 0 || bot.pos.y !== 0 || bot.pos.z !== 0)
+    if (bot.pos.x !== 0 || bot.pos.y !== 0 || bot.pos.z !== 0)
       throw "Not in origin"
 
-    if(state.getBotsNum() !== 1)
+    if (state.getBotsNum() !== 1)
       throw "Not only bot"
 
-    if(state.harmonics !== 0)
+    if (state.harmonics !== 0)
       throw "Not low harmonics"
 
   }
 
-  toString() : string {
-      return "Halt"
+  toString(): string {
+    return "Halt"
   }
 }
 
 export class Wait {
-  run(state : State, bid : number) {
+  run(state: State, bid: number) {
     // nothing to do here!
   }
 
-  toString() : string {
+  toString(): string {
     return "Wait"
   }
 }
 
 export class Flip {
-  run(state : State, bid : number) {
+  run(state: State, bid: number) {
     state.harmonics = state.harmonics === 1 ? 0 : 1
   }
 
-  toString() : string {
+  toString(): string {
     return "Flip"
   }
 }
 
 export class SMove {
-  constructor(lld : Coord) {
-    if(!lld.isLongLinearDiff())
+  constructor(lld: Coord) {
+    if (!lld.isLongLinearDiff())
       throw "Not a lld"
 
     this.lld = lld;
@@ -86,23 +86,23 @@ export class SMove {
     let bot = state.getBot(bid)
 
     let c = bot.pos.getAdded(this.lld)
-    if(!state.matrix.isValidCoord(c))
+    if (!state.matrix.isValidCoord(c))
       throw "Not valid coord"
 
     bot.pos = c
-    state.spendEnergy(this.lld.getMlen()*2)
+    state.spendEnergy(this.lld.getMlen() * 2)
   }
 
-  toString() : string {
+  toString(): string {
     return `SMove ${this.lld}`;
   }
 }
 
 export class LMove {
-  constructor(sld1 : Coord, sld2 : Coord) {
-    if(!sld1.isShortLinearDiff())
+  constructor(sld1: Coord, sld2: Coord) {
+    if (!sld1.isShortLinearDiff())
       throw "1 is not a sld"
-    if(!sld2.isShortLinearDiff())
+    if (!sld2.isShortLinearDiff())
       throw "2 is not a sld"
 
     this.sld1 = sld1;
@@ -113,72 +113,77 @@ export class LMove {
     let bot = state.getBot(bid)
 
     let c1 = bot.pos.getAdded(this.sld1)
-    if(!state.matrix.isValidCoord(c1))
+    if (!state.matrix.isValidCoord(c1))
       throw "C1 is not valid coord"
 
     let c2 = c1.getAdded(this.sld2)
-    if(!state.matrix.isValidCoord(c2))
+    if (!state.matrix.isValidCoord(c2))
       throw "C2 is not valid coord"
 
     bot.pos = c2
-    state.spendEnergy((this.sld1.getMlen() + 2 + this.sld2.getMlen())*2)
+    state.spendEnergy((this.sld1.getMlen() + 2 + this.sld2.getMlen()) * 2)
   }
 
-  toString() : string {
+  toString(): string {
     return `LMove ${this.sld1} ${this.sld2}`;
   }
 }
 
 export class FusionP {
-  constructor(nd : Coord) {
+  constructor(nd: Coord) {
     this.nd = nd;
   }
 
-  run(state : State, bid : number) { throw "unsupported" }
+  run(state: State, bid: number) {
+    throw "unsupported"
+  }
 
-  toString() : string {
+  toString(): string {
     return "FusionP"
   }
 
 }
 
 export class FusionS {
-  constructor(nd : Coord) {
+  constructor(nd: Coord) {
     this.nd = nd;
   }
 
-  run(state : State, bid : number) { throw "unsupported" }
+  run(state: State, bid: number) {
+    throw "unsupported"
+  }
 
-  toString() : string {
+  toString(): string {
     return "FusionS"
   }
 
 }
 
 export class Fission {
-  constructor(nd : Coord, m : number) {
+  constructor(nd: Coord, m: number) {
     this.nd = nd;
     this.m = m;
   }
 
-  run(state : State, bid : number) {}
+  run(state: State, bid: number) {
+  }
 
-  toString() : string {
+  toString(): string {
     return "Fission"
   }
 }
 
 export class Fill {
-  constructor(nd : Coord) {
-    if(!nd.isNearCoordDiff())
+  constructor(nd: Coord) {
+    if (!nd.isNearCoordDiff())
       throw "Not a nd"
     this.nd = nd.getCopy();
   }
 
-  run(state : State, bid : number) {
+  run(state: State, bid: number) {
     let bot = state.getBot(bid)
     let c = bot.pos.getAdded(this.nd)
-    if(!state.matrix.isValidCoord(c))
+    if (!state.matrix.isValidCoord(c))
       throw "Not valid coord"
 
     let isAlreadyFilled = state.matrix.isFilled(c.x, c.y, c.z)
@@ -187,22 +192,22 @@ export class Fill {
     state.spendEnergy(isAlreadyFilled ? 6 : 12)
   }
 
-  toString() : string {
+  toString(): string {
     return `Fill ${this.nd}`;
   }
 }
 
 export class Void {
-  constructor(nd : Coord) {
-    if(!nd.isNearCoordDiff())
+  constructor(nd: Coord) {
+    if (!nd.isNearCoordDiff())
       throw "Not a nd"
     this.nd = nd;
   }
 
-  run(state : State, bid : number) {
+  run(state: State, bid: number) {
     let bot = state.getBot(bid)
     let c = bot.pos.getAdded(this.nd)
-    if(!state.matrix.isValidCoord(c))
+    if (!state.matrix.isValidCoord(c))
       throw "Not valid coord"
 
     let isAlreadyFilled = state.matrix.isFilled(c.x, c.y, c.z)
@@ -211,34 +216,38 @@ export class Void {
     state.spendEnergy(isAlreadyFilled ? -12 : 3)
   }
 
-  toString() : string {
-    return "Void<"+nd+">"
+  toString(): string {
+    return `Void $<{this.nd}>`
   }
 }
 
 export class GFill {
-  constructor(nd : Coord, fd : Coord) {
+  constructor(nd: Coord, fd: Coord) {
     this.nd = nd;
     this.fd = fd;
   }
 
-  run(state : State, bid : number) { throw "unsupported" }
+  run(state: State, bid: number) {
+    throw "unsupported"
+  }
 
-  toString() : string {
+  toString(): string {
     return "GFill"
   }
 
 }
 
 export class GVoid {
-  constructor(nd : Coord, fd : Coord) {
+  constructor(nd: Coord, fd: Coord) {
     this.nd = nd;
     this.fd = fd;
   }
 
-  run(state : State, bid : number) { throw "unsupported" }
+  run(state: State, bid: number) {
+    throw "unsupported"
+  }
 
-  toString() : string {
+  toString(): string {
     return "GVoid"
   }
 
--- test/solve.test.js
+++ test/solve.test.js
@@ -2,13 +2,13 @@ import Solver from "../src/solve";
 import { readModel } from "../src/model/reader";
 
 test("solve", () => {
-  const m = readModel('./problemsL/LA001_tgt.mdl')
+  const m = readModel('./problemsL/LA186_tgt.mdl')
 
   let solution = new Solver().solve(m)
 
   // console.log(solution.toString());
-  // console.log(`Required energy: ${solution.state.energy}`);
+  // console.log(`Required steps: ${solution.commands.length}, energy: ${solution.state.energy}`);
 
-//  expect(solution.commands.length).toBe(1564)
+  // expect(solution.commands.length).toBe(1564)
 
 });
