9f41e75e6d676bb471a0d936e59a7d7c7cc391fd
Dmitry Boyko <dmitry@thebodva.com>
2018-07-21 23:52:14 +0300

    update runner and uploader

--- runner.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-/usr/bin/env yarn start
-for i in {001..186}
-do
-    num=$(printf "%03d" $i)
-    echo $num
-    /usr/bin/env node dist/index.js $1 $2 $num
-done
--- upload_icfp.sh
+++ upload_icfp.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 set -e
+yarn run launch
 privateID=e196b3421b5141ed8763a5837d0612ed
 timestamp=$(date +"%H%M%S")
 filename=submission$timestamp.zip
+
+cp -vnpr ./dfltTracesF/* ./$1
 cd $1
 zip --password $privateID $filename *
 mv $filename .. && cd ..
 sha=$(shasum -a 256 $filename | awk '{ print $1 }')
 scp $filename bodva@thebodva.com:/var/www/thebodva.com/www/upload
 curl -L \
-  --data-urlencode action=submit \
-  --data-urlencode privateID=$privateID \
-  --data-urlencode submissionURL=https://thebodva.com/upload/$filename \
-  --data-urlencode submissionSHA=$sha \
-  https://script.google.com/macros/s/AKfycbzQ7Etsj7NXCN5thGthCvApancl5vni5SFsb1UoKgZQwTzXlrH7/exec
+ --data-urlencode action=submit \
+ --data-urlencode privateID=$privateID \
+ --data-urlencode submissionURL=https://thebodva.com/upload/$filename \
+ --data-urlencode submissionSHA=$sha \
+ https://script.google.com/macros/s/AKfycbzQ7Etsj7NXCN5thGthCvApancl5vni5SFsb1UoKgZQwTzXlrH7/exec
 rm -rf $filename
